99c1080cd80bc1bd1834f3edc8b1f1837f82c319
Tomoya Watanabe <tomoya@ryogeisya.jp>
2019-02-13 18:21:18 +0900

    modify: buildのcssやjsのディレクトリを修正

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/gallery/praise_girl/',
 
     /**
      * Source Maps
